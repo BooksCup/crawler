f2904aaf83a71c6375a33ba30b3315deeb95df43
BooksCup <812809680@qq.com>
2020-08-19 12:00:18 +0800

    做去重处理

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import datetime
+import time, datetime
 import uuid
 
 import pymysql
@@ -26,18 +26,46 @@ class CrawlerPipeline(object):
 
     def process_item(self, item, spider):
         create_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        sql = 'insert into t_weave_price(price_id, price_type, price_name, price_last_trade, price_unit, price_change, price_date, price_create_time)' \
-              ' values(%s, %s, %s, %s, %s, %s, %s, %s)'
-        data = (
-            str(uuid.uuid1()).replace("-", ""),
-            item['type'],
-            item['name'],
-            item['lastTrade'],
-            item['unit'],
-            item['change'],
-            item['date'],
-            create_time
-        )
-        self.cur.execute(sql, data)
-        self.client.commit()
+
+        self.cur.execute("select 1 from t_weave_price_copy where price_type = %s and price_name = %s and price_date = %s",
+                         (item['type'], item['name'],item['date']))
+        result = self.cur.fetchone()
+        if result:
+            print('数据已存在')
+        else:
+            sql = 'insert into t_weave_price_copy(price_id, price_type, price_name, price_last_trade, price_unit, price_change, price_date, price_create_time)' \
+                  ' values(%s, %s, %s, %s, %s, %s, %s, %s)'
+            data = (
+                str(uuid.uuid1()).replace("-", ""),
+                item['type'],
+                item['name'],
+                item['lastTrade'],
+                item['unit'],
+                item['change'],
+                item['date'],
+                create_time
+            )
+            self.cur.execute(sql, data)
+            self.client.commit()
+
+        self.cur.execute("select 1 from t_trade_moments where moments_type = %s and moments_date = %s",
+                         ('0', item['date']))
+        result = self.cur.fetchone()
+        if result:
+            print('数据已存在')
+        else:
+            date_pretty = str(datetime.datetime.now().month) + '月' + str(datetime.datetime.now().day) + '日'
+            moments_data = (
+                str(uuid.uuid1()).replace("-", ""),
+                '0',
+                '',
+                date_pretty + ' 原材料行情',
+                '包括化纤 纱线的今日价格',
+                item['date'],
+                create_time
+            )
+            self.cur.execute(
+                "insert into t_trade_moments(moments_id, moments_type, moments_image, moments_title, moments_content, moments_date, moments_create_time) values(%s, %s, %s, %s, %s, %s, %s)",
+                moments_data)
+            self.client.commit()
         return item
